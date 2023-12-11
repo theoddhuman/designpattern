2b957e15261cc47a48b9ee4f12a3f903939aacee
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2023-12-12 01:23:47 +0530

    builder outer pattern

---
+++ src/main/java/com/subham/designpattern/builder/outer/UserDto.java
@@ -0,0 +1,12 @@
+package com.subham.designpattern.builder.outer;
+
+/**
+ * @author subham.paul
+ */
+public interface UserDto {
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+}
